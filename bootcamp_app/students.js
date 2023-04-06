157811ac5d2ef1022faa0d50dad3019a0b4c822b
RorySchof <roryschofield@gmail.com>
2023-04-06 08:17:44 -0400

    Protected against sql injection

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -7,15 +7,78 @@ const client = new Client({
   database: 'bootcampx'
 });
 
-pool.query(`
+const cohortName = process.argv[2];
+const limit = process.argv[3] || 5;
+
+const queryString = `
 SELECT students.id as student_id, students.name as name, cohorts.name as cohort
 FROM students
-JOIN cohorts ON cohorts.id = cohort_id
-WHERE cohorts.name LIKE '%${process.argv[2]}%'
-LIMIT ${process.argv[3] || 5};
-`)
-.then(res => {
-  res.rows.forEach(user => {
-    console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
+JOIN cohorts ON cohorts.id = students.cohort_id
+WHERE cohorts.name LIKE $1
+LIMIT $2;
+`;
+
+const values = [`%${cohortName}%`, limit];
+
+client.query(queryString, values)
+  .then(res => {
+    res.rows.forEach(user => {
+      console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
+    });
   })
-}).catch(err => console.error('query error', err.stack));
+  .catch(err => console.error('query error', err.stack));
+
+
+
+
+
+
+
+// const { Client } = require('pg');
+
+// const client = new Client({
+// user: 'vagrant',
+// password: '123',
+// host: 'localhost',
+// database: 'bootcampx'
+// });
+
+// const cohortName = process.argv[2];
+// const limit = process.argv[3] || 5;
+
+// const queryString = SELECT students.id as student_id, students.name as name, cohorts.name as cohort FROM students JOIN cohorts ON cohorts.id = cohort_id WHERE cohorts.name LIKE $1 LIMIT $2;;
+
+// const values = [%${cohortName}%, limit];
+
+// client.query(queryString, values)
+// .then(res => {
+// res.rows.forEach(user => {
+// console.log(${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort);
+// });
+// })
+// .catch(err => console.error('query error', err.stack)
+
+
+
+
+// const { Client } = require('pg');
+
+// const client = new Client({
+//   user: 'vagrant',
+//   password: '123',
+//   host: 'localhost',
+//   database: 'bootcampx'
+// });
+
+// pool.query(`
+// SELECT students.id as student_id, students.name as name, cohorts.name as cohort
+// FROM students
+// JOIN cohorts ON cohorts.id = cohort_id
+// WHERE cohorts.name LIKE '%${process.argv[2]}%'
+// LIMIT ${process.argv[3] || 5};
+// `)
+// .then(res => {
+//   res.rows.forEach(user => {
+//     console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
+//   })
+// }).catch(err => console.error('query error', err.stack));
